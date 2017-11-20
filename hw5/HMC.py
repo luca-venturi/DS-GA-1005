c6ddf91bceb21da83645d8645b254abe27ece93a
luca-venturi <luca.venturi.92@gmail.com>
2017-11-20 18:29:04 -0500

    hw5 to run

--- hw5/HMC.py
+++ hw5/HMC.py
@@ -1,40 +1,77 @@
 # Hamiltonian Monte Carlo simulations of X ~ N(0,S)
-# S = [[1,0.998],[0.998,1]]
+# covariance: S = [[1,0.998],[0.998,1]]
+# A ~ S^{-1}
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-cov = [[1.,0.998],[0.998,1.]]
-A = [[250.25, -249.75],[-249.75, 250.25]] # approximate of cov^{-1}
-n = A.shape[0]
+def genTraj(x,_iter):
+	path = [x]
+	x_values = [x]
+	x = np.array(x)
+	A = np.array([[250.25, -249.75],[-249.75, 250.25]])
+	n = A.shape[0]	
 
-def E(x):
-	return np.dot(x,np.dot(A,x)) / 2.
+	def evalE(_x):
+		return np.dot(_x,np.dot(A,_x)) / 2.
 	
-def G(x):
-	return np.dot(A,x)
-
-x = [-1.,1.5]
-_iter = 20
-eps = 0.3
-step = 12
-for i in range(_iter):
-	p = np.random.randn(n)
-	H = np.dot(p,p) / 2. + E(x)
+	def evalG(_x):
+		return np.dot(A,_x)
 	
-	x_new = x
-	G_new = G(x)
+	eps = 0.055
+	step = 19
+	for i in range(_iter):
+		p = np.random.randn(n)
+		H = np.dot(p,p) / 2. + evalE(x)
 	
-	for tau in range(step):
-		p -= eps * G_new / 2.
-		x_new += eps * p
-		G_new = G(x_new)
-		p -= eps * G_new / 2.
+		x_new = x
+		G_new = evalG(x)
 		
-	E_new = E(x_new)
-	H_new = np.dot(p,p) / 2. + E_new
-	dH = H_new - H
+		tmp = []
+		for tau in range(step):
+			p -= eps * G_new / 2.
+			x_new += eps * p
+			print x_new
+			tmp.append([x_new[0],x_new[1]])
+			G_new = evalG(x_new)
+			p -= eps * G_new / 2.
+		
+		E_new = evalE(x_new)
+		H_new = np.dot(p,p) / 2. + E_new
+		dH = H - H_new
 	
-	if np.random.rand() < np.exp(-dH):
-		G = G_new
-		x = x_new
-		E = E_new
+		u = np.random.rand()
+		if u < np.exp(dH):
+			G = G_new
+			x = x_new
+			E = E_new
+			print tmp
+			for tau in range(step):
+				path.append(tmp[tau])
+			x_values.append([x[0],x[1]])
+
+	return np.array(path), np.array(x_values)
+
+def plotTraj(x,v):
+	A = np.array([[250.25, -249.75],[-249.75, 250.25]])
+	c = 1. / A[0,0]
+	b = A[0,1] * c * 2
+	xl = np.sqrt( 4 * c / (4 - b**2))
+	xp = np.linspace(-xl,xl,200) 
+	ypu = (- b * xp + np.sqrt(b**2 * xp**2 + 4 * (c - xp**2))) * 0.5
+	ypb = (- b * xp - np.sqrt(b**2 * xp**2 + 4 * (c - xp**2))) * 0.5
+	plt.plot(xp,ypu,'b--')
+	plt.plot(xp,ypb,'b--')
+	plt.gca().set_aspect('equal', adjustable='box')
+	plt.xlim(min(-1.,np.min(x[:,0])), max(1.,np.max(x[:,0])))
+	plt.ylim(min(-1.,np.min(x[:,1])), max(1.,np.max(x[:,1])))
+	plt.plot(x[:,0],x[:,1],'r-')
+	plt.plot(v[:,0],v[:,1],'ro')
+	plt.show()
+
+# simulate
+
+start = input('Enter start value: ')
+_iter = input('Enter number of iterations: ')
+x,v = genTraj(start,_iter)
+plotTraj(x,v)
